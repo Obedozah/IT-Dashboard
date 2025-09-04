637c6d3ce644cddcf02fa9a34fc1fb8e93404385
Avery Lopez <averyagustin05@gmail.com>
2025-09-04 10:44:08 -0700

    Finished SystemInfo Function

--- Scripts/gatherSystemInfo.py
+++ Scripts/gatherSystemInfo.py
@@ -3,31 +3,34 @@ import platform
 import socket
 import uuid
 
+hostname = socket.gethostname()
+
 def gather_system_info():
-    systemInfo = {
-        "os" : {
-            "platform": platform.system(),
-            "platformRelease": platform.release(),
-            "platformVersion": platform.version()
-        },
-        "cpu": {
-            "physicalCores": psutil.cpu_count(),
-            "logicalCores": psutil.cpu_count(logical=True),
-            "frequency": psutil.cpu_freq()._asdict(),
-            "usagePercent": psutil.cpu_percent(interval=1)
-        },
-        "memory": {
-            "total": psutil.virtual_memory().total,
-            "available": psutil.virtual_memory().available,
-            "used": psutil.virtual_memory().used,
-            "percent": psutil.virtual_memory().percent
+        systemInfo = {
+            "os" : {
+                "platform": platform.system(),
+                "platformRelease": platform.release(),
+                "platformVersion": platform.version()
+            },
+            "cpu": {
+                "physicalCores": psutil.cpu_count(),
+                "logicalCores": psutil.cpu_count(logical=True),
+                "frequency": psutil.cpu_freq()._asdict(),
+                "usagePercent": psutil.cpu_percent(interval=1)
+            },
+            "memory": {
+                "total": psutil.virtual_memory().total,
+                "available": psutil.virtual_memory().available,
+                "used": psutil.virtual_memory().used,
+                "percent": psutil.virtual_memory().percent
+            },
+            "networkInfo": {
+            "hostname": hostname,
+            "ipAddress": socket.gethostbyname(hostname),
+            "macAddress": getMacAddress()
+            }
         }
-    }
-    networkInfo = {
-        "hostname": socket.gethostname(),
-        "ipAddress": socket.gethostbyname(networkInfo.hostname),
-        "macAddress": getMacAddress()
-    }
+        return systemInfo
 
 def getMacAddress():
     macAddressBits = uuid.getnode()
