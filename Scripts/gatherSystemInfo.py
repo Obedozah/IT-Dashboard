6409ab2a8825b59cd3159fbfc9366de2146fd84a
Avery Lopez <averyagustin05@gmail.com>
2025-09-04 14:14:53 -0700

    Updated gatherSystemInfo to keep raw system data

--- Scripts/gatherSystemInfo.py
+++ Scripts/gatherSystemInfo.py
@@ -12,28 +12,24 @@ def gather_system_info():
                 "platformRelease": platform.release(),
                 "platformVersion": platform.version()
             },
-            "cpu": {
-                "physicalCores": psutil.cpu_count(),
-                "logicalCores": psutil.cpu_count(logical=True),
-                "frequency": psutil.cpu_freq()._asdict(),
-                "usagePercent": psutil.cpu_percent(interval=1)
-            },
-            "memory": {
-                "total": psutil.virtual_memory().total,
-                "available": psutil.virtual_memory().available,
-                "used": psutil.virtual_memory().used,
-                "percent": psutil.virtual_memory().percent
+            "hardwareHealth": {
+                "cpu": {
+                    "physicalCores": psutil.cpu_count(),
+                    "logicalCores": psutil.cpu_count(logical=True),
+                    "frequency": psutil.cpu_freq()._asdict(),
+                    "usagePercent": psutil.cpu_percent(interval=1)
+                },
+                "memory": {
+                    "total": psutil.virtual_memory().total,
+                    "available": psutil.virtual_memory().available,
+                    "used": psutil.virtual_memory().used,
+                    "percent": psutil.virtual_memory().percent
+                }
             },
             "networkInfo": {
-            "hostname": hostname,
-            "ipAddress": socket.gethostbyname(hostname),
-            "macAddress": getMacAddress()
+                "hostname": hostname,
+                "ipAddress": socket.gethostbyname(hostname),
+                "macAddress": uuid.getnode()
             }
         }
         return systemInfo
-
-def getMacAddress():
-    macAddressBits = uuid.getnode()
-    macAddressHex = '%012x' % macAddressBits
-    macAddress = ':'.join(macAddressHex[i:i+2] for i in range(0, 12, 2))
-    return macAddress
