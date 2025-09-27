548d0ea5e148ffa4fc17eb1427d55dfd52f58784
Avery Lopez <averyagustin05@gmail.com>
2025-09-26 19:41:16 -0700

    Added testing scripts with directory

--- Scripts/main.py
+++ Scripts/main.py
@@ -4,11 +4,6 @@ from scripts.network_scanner.local_arp import reader
 
 if __name__ == "__main__":
     system_info = gsi.gather_system_info()
-<<<<<<< HEAD
-    print(system_info)
-    print("\n")
-=======
     print(json.dumps(system_info, indent = 2))
->>>>>>> feature/gsi
     local_arp = reader.read_arp_table()
     print(local_arp)
