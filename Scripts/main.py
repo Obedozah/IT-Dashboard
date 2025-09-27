58b112e4e34880ab06e7a377e1e0281811d1c66c
Avery Lopez <averyagustin05@gmail.com>
2025-09-26 18:34:09 -0700

    Changed printing format in main

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,6 +2,6 @@ from scripts.system_info import gather_system_info as gsi
 from scripts.network_scanner.local_arp import reader
 
 if __name__ == "__main__":
-    system_info = gsi.gather_system_info()
-    print(system_info)
+    print(gsi.gather_system_info())
+    print("\n")
     print(reader.read_arp_table())
