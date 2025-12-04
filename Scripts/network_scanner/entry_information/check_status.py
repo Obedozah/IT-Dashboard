6f703c097dd1f9f57f026b49fc7dde0669ff7c22
Avery Lopez <averyagustin05@gmail.com>
2025-12-03 21:11:07 -0800

    Implemented ARP Sweeping, Changed ping subprocess to icmplib, removed local arp scanning

--- Scripts/network_scanner/entry_information/check_status.py
+++ Scripts/network_scanner/entry_information/check_status.py
@@ -1,20 +1,15 @@
-import subprocess
-from scripts.system_info import gather_system_info as gsi
-import re
-
-system_info = gsi.gather_system_info()
-platform = system_info['os']['platform'].lower()
+import icmplib
+from icmplib import ping
 
 def check_status(ip):
-    if platform.lower() == 'windows':
-        command = ['ping', '-n', '1', ip]
-    else:
-        command = ['ping', '-c', '1', ip]
 
-    status = subprocess.run(command, capture_output=True, text=True)
-    if status.returncode == 0:
-        ttl_line = re.search(r'ttl=(\d+)', status.stdout.lower())
-        ttl = ttl_line.group(1)
-        return "Online", ttl
-    else:
-        return "Offline", "N/A"
+    # Ping Status for IP
+    try:
+        ping_status = ping(ip, count=4, timeout=0.1, privileged=False)
+        if ping_status.is_alive:
+            return "Online"
+        else:
+            return "Offline"
+    except icmplib.exceptions.ICMPLibError as e:
+        print(f"Error Pinging: {e}")
+        return "Offline"
