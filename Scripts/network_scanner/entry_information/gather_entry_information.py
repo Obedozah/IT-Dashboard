1bdeffd7f991cf66e58eeca72e233345af28d369
Avery Lopez <averyagustin05@gmail.com>
2025-12-04 13:46:15 -0800

    Got Rid of Some Time Measuring in network_scan scripts

--- Scripts/network_scanner/entry_information/gather_entry_information.py
+++ Scripts/network_scanner/entry_information/gather_entry_information.py
@@ -1,4 +1,3 @@
-import time
 import socket
 from scripts.network_scanner.entry_information.vendor_lookup import vendor_lookup
 from scripts.network_scanner.entry_information.check_status import check_status
@@ -6,7 +5,6 @@ from scripts.network_scanner.entry_information.scan_ports import scan_ports
 from concurrent.futures import ThreadPoolExecutor
 
 def gather_entry_information(entry_data):
-    curr_time = time.time()
 
     # Vendor Lookup
     for entry in entry_data:
@@ -15,12 +13,8 @@ def gather_entry_information(entry_data):
         except socket.herror:
             entry["hostname"] = "N/A"
         entry["vendor"] = vendor_lookup(entry["mac"])
-    end_time = time.time()
-    elapsed_time = end_time - curr_time
-    print(f"Hostname/Vendor Lookup Time: {elapsed_time} seconds")
-    
+
     # Status/TTL with Threading
-    curr_time = time.time()
     ping_futures = []
     with ThreadPoolExecutor(max_workers=10) as exec:
         for entry in entry_data:
@@ -28,12 +22,8 @@ def gather_entry_information(entry_data):
 
         for entry, future in zip(entry_data, ping_futures):
             entry["ping_status"] = future.result()
-    end_time = time.time()
-    elapsed_time = end_time - curr_time
-    print(f"Status Check Time: {elapsed_time} seconds")
-
+    
     # Port Scanning with Threading
-    curr_time = time.time()
     portscan_futures = []
     with ThreadPoolExecutor(max_workers=10) as exec:
         for entry in entry_data:
@@ -41,9 +31,5 @@ def gather_entry_information(entry_data):
 
         for entry, future in zip(entry_data, portscan_futures):
             entry["open_ports"], entry["closed_ports"] = future.result()
-
-    end_time = time.time()
-    elapsed_time = end_time - curr_time
-    print(f"Port Scanning Time: {elapsed_time} seconds")
 
     return entry_data
