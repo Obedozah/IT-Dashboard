0f1c9de6bff6b802f82fcf4967a46b4c873596aa
Avery Lopez <averyagustin05@gmail.com>
2025-12-07 23:55:27 -0800

    Added CORS library to fix dev issues

--- Scripts/app.py
+++ Scripts/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from scripts.system_info import gather_system_info as gsi
 from scripts.network_scanner.full_scan import full_scan
 
 app = Flask(__name__)
+CORS(app)
 os, hardware_health, network_info = gsi.gather_system_info()
 
 @app.route('/device', methods= ["GET"])
--- frontend/src/components/dashboard/sections/network-scanner/panels/MetaData.jsx
+++ frontend/src/components/dashboard/sections/network-scanner/panels/MetaData.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
 import './MetaData.css';
 
 function MetaData() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function getDeviceData() {
+            try {
+                const response = await fetch('http://127.0.0.1:5000/device');
+                const jsonData = await response.json();
+                setData(jsonData);
+                console.log(jsonData);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        getDeviceData();
+    }, []);
+
     return (
-        <section className="meta-data">
-            Meta Data
-        </section>
-    )
+        <pre>{data ? JSON.stringify(data, null, 2) : 'Loading...'}</pre>
+    );
 }
 
 export default MetaData;
